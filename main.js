84be7cf3818e1c64f760aeb780c312ddba12874d
yourbreathtaking <52409746+yourbreathtaking@users.noreply.github.com>
2024-12-03 20:24:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import Tile from 'ol/Tile';
 const source = new GeoTIFF({
   sources: [
     {
-      url: 'ookedoikewoifkok.tif',
+      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
     },
   ],
 });
