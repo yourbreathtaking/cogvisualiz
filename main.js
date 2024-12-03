eb3e194588b0c41fca6e3ccde6e2db2eca9cd410
yourbreathtaking <aaaminedrissi@gmail.com>
2024-12-03 18:58:43 +0100

    new files and folders

---
+++ main.js
@@ -0,0 +1,28 @@
+import GeoTIFF from 'ol/source/GeoTIFF.js';
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+import OSM from'ol/source/OSM.js';
+import Tile from 'ol/Tile';
+
+
+
+const source = new GeoTIFF({
+  sources: [
+    {
+      url: 'ookedoikewoifkok.tif',
+    },
+  ],
+});
+const osmLayer = new TileLayer({
+  source: new OSM(),
+});
+const map = new Map({
+  target: 'map',
+  layers: [osmLayer,
+    
+    new TileLayer({
+      source: source,
+    })
+  ],
+  view: source.getView(),
+});
